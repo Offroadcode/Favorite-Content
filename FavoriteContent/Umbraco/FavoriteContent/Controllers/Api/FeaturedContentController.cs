96786d1a33274126cd5d474ea49c684e70ab6251
Janae Cram <janae@offroadcode.com>
2019-03-01 10:44:31 -0800

    Set up the project & set up dummy endpoints
    * Added the C# project
    * Installed Umbraco and the references
    * Set up the grunt MSBuild
    * Added dummy endpoints based on Kyle's request so he can wire it in on the front
    
    https://trello.com/c/SI11hmnr/113-common-fields-aka-favorite-content

---
+++ FavoriteContent/Umbraco/FavoriteContent/Controllers/Api/FeaturedContentController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Http;
+using System.Web.Http.Results;
+using Umbraco.Web.WebApi;
+
+namespace FavoriteContent.Controllers.Api
+{
+    public class FavoriteContentController : UmbracoAuthorizedApiController
+    {
+        [HttpGet]
+        public JsonResult<string[]> GetFavoriteProperties()
+        {
+            var favorites = new string[] { };
+
+            return Json(favorites);
+        }
+
+        [HttpGet]
+        public bool AddPropertyToFavorites(string property)
+        {
+            return true;
+        }
+
+        [HttpGet]
+        public bool RemovePropertyFromFavorites(string property)
+        {
+            return true;
+        }
+    }
+}
